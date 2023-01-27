70627e7f19ac1d8d66116db321846457e52a8082
bornoflightning <victor.escarcega@hotmail.com>
2023-01-26 23:36:00 -0700

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 //port variable
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // express as a function
 const app = express();
